cd9fc285d45494a7d5b750587602d19efd50b4f0
Johann Miller <jkmiller@umd.edu>
2017-07-26 15:07:41 -0400

    capitalization error

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ main()
 {
 	# set default path unless environment var exists
 	if [ ! -n "${!OPEN_SAT_KIT_PATH}" ]; then
-		OPEN_SAT_KIT_PATH="`pwd`/OpenSatKit/"
+		OPEN_SAT_KIT_PATH="`pwd`/OpenSatKit-master/"
 	fi
 	announce "Installing OpenSatKit v$OPEN_SAT_KIT_VERSION in \"$OPEN_SAT_KIT_PATH\""
 	confirm "Continue?" 1
@@ -15,7 +15,7 @@ main()
 	curl -LO https://github.com/OpenSatKit/OpenSatKit/archive/master.tar.gz
 	tar -xzf master.tar.gz
 	rm master.tar.gz
-	cd OpenSatkit-master
+	cd OpenSatKit-master
 
 	announce "Installing dependencies"
   cmdtoapt="apt-get update -y; apt-get install -y cmake; apt-get install -y curl; apt-get install -y default-jre; apt-get install -y gcc-multilib; apt-get install -y xfce4-terminal; apt-get install -y gcc; apt-get install -y g++; apt-get install -y libssl-dev; apt-get install -y libyaml-dev; apt-get install -y libffi-dev; apt-get install -y libreadline6-dev; apt-get install -y zlib1g-dev; apt-get install -y libgdbm3; apt-get install -y libgdbm-dev; apt-get install -y libncurses5-dev; apt-get install -y git; apt-get install -y libgstreamer0.10-dev; apt-get install -y libgstreamer-plugins-base0.10-dev; apt-get install -y cmake; apt-get install -y freeglut3; apt-get install -y freeglut3-dev; apt-get install -y qt4-default; apt-get install -y qt4-dev-tools; apt-get install -y libsmokeqt4-dev;"
