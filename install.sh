d329a31cc5b171b9c255a9f3a2e67e1c406215b7
David McComas <david.c.mccomas@nasa.gov>
2019-01-20 07:53:31 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 OPEN_SAT_KIT_VERSION="1.6.0"
+BASE_DIR="$(pwd)"
 
 # Intentional order: OSK Archive, Dependencies, COSMOS, Build System 
 # - Dependencies cover OSK's needs, COSMOS may need additional ones
@@ -19,10 +20,22 @@ fi
 announce "Installing OpenSatKit v$OPEN_SAT_KIT_VERSION in \"$OPEN_SAT_KIT_PATH\""
 confirm "Continue?" 1
 
-announce "Downloading OSK archive zip file from \"https://github.com/OpenSatKit/OpenSatKit/archive/master.tar.gz\""
-wget -c https://github.com/OpenSatKit/OpenSatKit/archive/master.tar.gz
-tar -xzf master.tar.gz
-rm master.tar.gz
+announce "Installing OSK archive zip file from \"https://github.com/OpenSatKit/OpenSatKit/archive/master.tar.gz\""
+if [ -d $BASE_DIR/OpenSatKit-master ]; then
+   read -p "OpenSatKit-master directory already exists. Delete for clean install? (Yn): " -n 1 -r
+   echo
+   if [[ $REPLY =~ ^[Yy]?$ ]]; then
+      rm -rf $BASE_DIR/OpenSatKit-master
+   fi
+fi
+
+if [ ! -d $BASE_DIR/OpenSatKit-master ]; then
+   wget -c https://github.com/OpenSatKit/OpenSatKit/archive/master.tar.gz
+   tar -xzf master.tar.gz
+   rm master.tar.gz
+else
+   echo "Preserving existing OpenSatKit-master. Continuing installation..."
+fi
 
 announce "Installing dependencies"
 cmdtoapt="apt-get update -y; apt-get install -y cmake; apt-get install -y curl; apt-get install -y default-jre; apt-get install -y gcc-multilib; apt-get install -y xfce4-terminal; apt-get install -y gcc; apt-get install -y g++; apt-get install -y libssl-dev; apt-get install -y libyaml-dev; apt-get install -y libffi-dev; apt-get install -y libreadline6-dev; apt-get install -y zlib1g-dev; apt-get install -y libgdbm3; apt-get install -y libgdbm-dev; apt-get install -y libncurses5-dev; apt-get install -y git; apt-get install -y libgstreamer0.10-dev; apt-get install -y libgstreamer-plugins-base0.10-dev; apt-get install -y freeglut3; apt-get install -y freeglut3-dev; apt-get install -y qt4-default; apt-get install -y qt4-dev-tools;apt-get install -y libqt4-dev; apt-get install -y libsmokeqt4-dev; apt-get install libcanberra-gtk*;apt install dos2unix; apt-get update -y;"
