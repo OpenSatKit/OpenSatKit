d338adcc596d908552d94af2f5b52bc0b5f6cc33
dmccomas <dmccomas63@gmail.com>
2021-08-09 06:35:19 -0700

    Completed remote pi-sat interface and documentation

---
+++ cosmos/config/targets/GPIO_DEMO/lib/gpio_demo_const.rb
@@ -0,0 +1,37 @@
+###############################################################################
+# Pi-Sat GPIO Demo Constants
+#
+# Notes:
+#   1. The demo app is only used with the Pi-Sat target
+#
+# License:
+#   Written by David McComas, licensed under the copyleft GNU General 
+#   Public License (GPL).
+# 
+################################################################################
+
+require 'osk_global'
+
+module GpioDemo
+
+   ##############################
+   ## Compile-time Definitions ##
+   ##############################
+
+
+   ###########################
+   ## Init file Definitions ##
+   ###########################
+
+
+   #######################
+   ## Table Definitions ##
+   #######################
+
+
+   ########################
+   ## Learning Resources ##
+   ########################
+
+   
+end # module GpioDemo
--- cosmos/lib/hw_target.rb
+++ cosmos/lib/hw_target.rb
@@ -17,6 +17,17 @@ module HwTarget
   PROCESSOR_ENDIAN = "LITTLE_ENDIAN"
    
   CPU_ADDR_SIZE = 4
-   
+  
+  #
+  # Only one of the following IDs should be uncommented
+  #   LOCAL      - Target running on same machine as COSMOS
+  #   PISAT      - Using a Rasberry Pi configured according to OSK's Pi-Sat Quick Start Guide
+  #   PISAT_DEMO - Special PiSat demo configuration supplied by Alan Cudmore
+  #
+  
+  ID = "LOCAL"  
+  #ID = "PISAT" 
+  #ID = "PISAT_DEMO" 
+
 end # module HwTarget
 
